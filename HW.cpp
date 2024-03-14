f2becf126f97e9aa20c9e09162ccdb0b5bceb19a
Artyom Pvblo <sotr.pvblo@mail.ru>
2024-03-14 21:26:00 +0300

    Добавьте файлы проекта.

---
+++ HW.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void Print_num(int N, int k)
+{
+    for (int i = k; i <= N; i += 2)
+        cout << i << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    int N;
+    cout << "N="; cin >> N;
+
+    cout << "Even:\n";
+    Print_num(N, 0);
+
+    cout << "Odd:\n";
+    Print_num(N, 1);
+
+    system("pause");
+    return 0;
+}
